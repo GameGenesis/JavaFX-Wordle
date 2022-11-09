e8f6fa4249a000c0cd1d67443bd7f03c1c1d46c2
GameGenesis <gamegenesisdev@gmail.com>
2022-11-09 12:28:37 -0500

    Implement backspace and enter controls
    * Backspace deletes the last letter
    
    * Enter submits the word if it is complete

--- src/main/java/AppController.java
+++ src/main/java/AppController.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
@@ -13,6 +15,32 @@ public class AppController {
     private HBox row1;
 
     public void getInput(String key) {
+        if (key.equals("BACK_SPACE")) {
+            List<Node> children = row1.getChildren();
+            for (int i = children.size() - 1; i >= 0; i--) {
+                Label label = (Label)children.get(i);
+
+                if (!label.getText().isEmpty()) {
+                    label.setText("");
+                    break;
+                }
+            }
+            return;
+        }
+
+        if (key.equals("ENTER")) {
+            for (Node child : row1.getChildren()) {
+                Label label = (Label)child;
+                
+                if (label.getText().isEmpty()) {
+                    return;
+                }
+            }
+            
+            System.out.println("Complete Word!");
+            return;
+        }
+
         if (key.length() != 1) {
             return;
         }
