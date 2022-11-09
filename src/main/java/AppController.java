5f444c5a8dcd99a14e14dcbae1860f6fa46c343e
GameGenesis <gamegenesisdev@gmail.com>
2022-11-09 13:08:19 -0500

    Update word submitting
    * Label box background, border, and text color change based on the letter
    
    * AppController now only references the parent VBox and gets each HBox as children of the VBox
    
    * Now supports multiple rows

--- src/main/java/AppController.java
+++ src/main/java/AppController.java
@@ -5,6 +5,8 @@ import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class AppController {
 
@@ -12,16 +14,29 @@ public class AppController {
     private AnchorPane lineAnchor;
 
     @FXML
-    private HBox row1;
+    private VBox letterBox;
+
+    private HBox rowBox;
+
+    private int currentRow = 0;
+    private String correctWord = "rainy";
 
     public void getInput(String key) {
+        List<Node> letterBoxChildren = letterBox.getChildren();
+        if (currentRow > letterBoxChildren.size() - 1)
+            return;
+
+        rowBox = (HBox)letterBoxChildren.get(currentRow);
+
         if (key.equals("BACK_SPACE")) {
-            List<Node> children = row1.getChildren();
+            List<Node> children = rowBox.getChildren();
             for (int i = children.size() - 1; i >= 0; i--) {
                 Label label = (Label)children.get(i);
 
                 if (!label.getText().isEmpty()) {
                     label.setText("");
+                    label.setId("letter-box-empty");
+                    label.setTextFill(Color.BLACK);
                     break;
                 }
             }
@@ -29,15 +44,32 @@ public class AppController {
         }
 
         if (key.equals("ENTER")) {
-            for (Node child : row1.getChildren()) {
-                Label label = (Label)child;
+            List<Node> children = rowBox.getChildren();
+            for (int i = children.size() - 1; i >= 0 ; i--) {
+                Label label = (Label)children.get(i);
+                String labelText = label.getText();
                 
-                if (label.getText().isEmpty()) {
+                if (labelText.isEmpty()) {
                     return;
+                }
+
+                if (labelText.equals(correctWord.substring(i, i+1).toUpperCase())) {
+                    label.setId("letter-box-correct-location");
+                    label.setTextFill(Color.WHITE);
+                }
+                else if (correctWord.toUpperCase().contains(labelText)) {
+                    label.setId("letter-box-wrong-location");
+                    label.setTextFill(Color.WHITE);
+                }
+                else {
+                    label.setId("letter-box-wrong-letter");
+                    label.setTextFill(Color.WHITE);
                 }
             }
             
             System.out.println("Complete Word!");
+            if (currentRow < 5)
+                currentRow++;
             return;
         }
 
@@ -50,11 +82,12 @@ public class AppController {
             return;
         }
 
-        for (Node child : row1.getChildren()) {
+        for (Node child : rowBox.getChildren()) {
             Label label = (Label)child;
             
             if (label.getText().isEmpty()) {
                 label.setText(key);
+                label.setId("letter-box-filled");
                 break;
             }
         }
